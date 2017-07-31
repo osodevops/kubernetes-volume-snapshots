c6173cfeeb1628e16d80f321233e6ca2bfea14df
BoySherman <mccullya@gmail.com>
2017-07-31 12:00:03 +0100

    Initial commit of kubernetes volume snapshot

---
+++ README.md
@@ -0,0 +1,20 @@
+Kubernetes Persistent Volume Backup / AWS
+==================================================================
+
+Requirements
+------------
+
+### AWS Secrets
+> kubectl create secret --namespace=dev generic aws-key-secret --from-file=config=$HOME/.aws/config --from-file=credentials=$HOME/.aws/credentials
+
+### Environment Variables
+
+
+
+How it works
+------------
+
+
+Usage
+------------
+
---
+++ kube_volume_snapshots/connection.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from kubernetes import client, config
+import boto3
+
+
+class Connection:
+
+    def __init__(self, config=None):
+        self.config = config
+        self.kubernetes = self.kubernetes_client()
+        self.s3_client = self.aws_s3_client()
+        self.s3_resource = self.aws_s3_resource()
+        self.e2_resource = self.aws_ec2_resource()
+
+    def kubernetes_client(self):
+        # K8 Clients
+        # Try pods crt files, and in not, look for $USER/.kube/config
+        try:
+            config.load_incluster_config()
+        except:
+            config.load_kube_config()
+
+        v1 = client.CoreV1Api()
+
+        return v1
+
+    def aws_s3_client(self):
+        s3_client = boto3.client('s3')
+        return s3_client
+
+    def aws_s3_resource(self):
+        s3_resource = boto3.resource('s3')
+        return s3_resource
+
+    def aws_ec2_resource(self):
+        ec2_resource = boto3.resource('ec2')
+        return ec2_resource
+
